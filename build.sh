47e25e71c33983982d1fc35fc2d4a558b4167e0b
Bernat Romagosa <bromagosa@e-citilab.eu>
2017-02-24 14:10:13 +0100

    added --watch parameter. Autobuilds on any file change.

--- build.sh
+++ build.sh
@@ -26,33 +26,65 @@
 #   You should have received a copy of the GNU General Public License
 #   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+# parse parameters
+while echo $1 | grep ^- > /dev/null; do eval $( echo $1 | sed 's/-//g' | sed 's/=.*//g' | tr -d '\012')=$( echo $1 | sed 's/.*=//g' | tr -d '\012'); shift; done
 
 # iterate over all .snp page descriptor files
-for page in `ls *.snp`; do
+function build() {
+    for page in `ls *.snp`; do
+        echo "Building $page..."
 
-    # create an html file with the same name as the descriptor file
-    html=www/"${page%.*}".html
-    rm -f $html
-    touch $html
+        # create an html file with the same name as the descriptor file
+        html=www/"${page%.*}".html
+        rm -f $html
+        touch $html
 
-    # process all descriptors in the descriptor file
-    while read -r descriptor
-    do
-        # find the template(s) matching the descriptor, possibly more
-        # than one per line, separated by semicolons
-        declare -a template_names="(${descriptor//;/ })";
-        rm -f tmp.html
-        for template in ${template_names[*]}; do
-            cat templates/$template.html >> tmp.html
+        # process all descriptors in the descriptor file
+        while read -r descriptor
+        do
+            # find the template(s) matching the descriptor, possibly more
+            # than one per line, separated by semicolons
+            declare -a template_names="(${descriptor//;/ })";
+            rm -f tmp.html
+            for template in ${template_names[*]}; do
+                cat templates/$template.html >> tmp.html
+            done
+
+            if grep -q @content $html; then
+                # replace the @content string for the contents of the template file
+                sed -e '/@content/ {' -e 'r tmp.html' -e 'd' -e '}' -i $html
+            else
+                cat tmp.html >> $html
+            fi
+        done < "$page"
+    done
+
+    rm -f tmp.html
+    echo "Done."
+}
+
+build
+
+# Watch and build on any file change
+if test -n $watch; then
+    declare -A lasttimes
+    while sleep 1; do
+        # ignores hidden files and dirs (./.*)
+        for file in `find -type f | grep -v "^\./\." | grep -v "./www"`; do
+            time=`stat -c %Z "$file"`
+
+            if [ -z ${lasttimes[$file]} ]; then
+                lasttimes["$file"]=$time
+            fi
+
+            if [ "$time" != "${lasttimes[$file]}" ]; then
+                echo "$file changed."
+                echo "Rebuilding..."
+                build
+                lasttimes["$file"]=$time
+                break
+            fi
+            lasttimes["$file"]=$time
         done
-
-        if grep -q @content $html; then
-            # replace the @content string for the contents of the template file
-            sed -e '/@content/ {' -e 'r tmp.html' -e 'd' -e '}' -i $html
-        else
-            cat tmp.html >> $html
-        fi
-    done < "$page"
-done
-
-rm -f tmp.html
+    done
+fi
