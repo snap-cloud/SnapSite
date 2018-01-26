046065ac9528c704406fa7e40935039a42cdfd25
Bernat Romagosa <bromagosa@e-citilab.eu>
2018-01-26 16:41:36 +0100

    reading cloud.js from Snap live instance

--- www/js/base.js
+++ www/js/base.js
@@ -1,4 +1,4 @@
-var snapURL = 'http://localhost:8082/snap.html', // location.protocol + '//bromagosa.github.io/Snap/snap.html';
+var snapURL = 'https://localhost:8082/snap.html', // location.protocol + '//bromagosa.github.io/Snap/snap.html';
     modules = [], // compatibility with cloud.js
     nop = function () {},
     localizer = new Localizer(),
